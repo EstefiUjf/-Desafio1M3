eb56969badb5c80e258d456ab875ff92c0584ef3
EstefiUjf <95715306+EstefiUjf@users.noreply.github.com>
2021-12-10 17:57:50 -0300

    Este es el desafio 1 del modulo 3

---
+++ g5_UjfalusiEstefania_D1.py
@@ -0,0 +1,9 @@
+
+print("hola mundo")
+def votar (nombre,edad):
+    if (edad) >= 16 :
+        return (nombre)+" " + "puede votar"
+    else:
+        return (nombre) +" "+ " debe esperar para votar"
+    
+
